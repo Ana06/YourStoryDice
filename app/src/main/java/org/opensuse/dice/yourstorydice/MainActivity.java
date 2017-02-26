46100a52f13a6fe8cc01d32df847b9fdd129ce67
Björn Geuken <bgeuken@suse.de>
2017-02-26 18:59:52 +0100

    Close DB connections when device activity closes
    There are a couple of events that cause the activity to close, eg. changing
    screen orientation, closing app, etc. . In such a case we need to close the
    db connection to prevent leakage.
    
    =====
    
    [200~W/SQLiteConnectionPool: A SQLiteConnection object for database
    '+data+user+0+org_opensuse_dice_yourstorydice+databases+YouStoryDice_db'
    was leaked!  Please fix your application to end transactions in progress
    properly and to close the database when it is no longer needed.

--- app/src/main/java/org/opensuse/dice/yourstorydice/MainActivity.java
+++ app/src/main/java/org/opensuse/dice/yourstorydice/MainActivity.java
@@ -88,6 +88,14 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    public void onDestroy() {
+        SQLiteDatabase readableDb = mDbHelper.getReadableDatabase();
+        readableDb.close();
+
+        super.onDestroy();
+    }
+
+    @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
